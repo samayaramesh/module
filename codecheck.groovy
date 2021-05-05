c8572095cb044f16f3324356dbf89d054052d26b
samayaramesh <samayaramesh@gmail.com>
2021-05-05 23:29:19 +0530

    Update codecheck.groovy

--- codecheck.groovy
+++ codecheck.groovy
@@ -1,12 +1,7 @@
-def call() {
-    timeout(time: 3, unit: 'MINUTES') {
-        echo "Initializing quality gates..."
-        sh 'sleep 10'
-        def result = waitForQualityGate()
-        if (result.status != 'OK') {
-             error "Pipeline aborted due to quality gate failure: ${result.status}"
-        } else {
-             echo "Quality gate passed with result: ${result.status}"
-        }
-    }
-}
+def call(Map stageParams) {
+
+    checkout([
+        $class: 'GitSCM', 
+        branches: [[name: stageParams.branch ]], 
+        userRemoteConfigs: [[url: stageParams.url ]]
+])
